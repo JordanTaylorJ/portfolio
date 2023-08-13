9d07a09cc038a0407a22342a8848d7d3376d2216
JordanJoseph <jordant.joseph@outlook.com>
2023-08-13 15:56:22 -0600

    remove default values from global css, create columns on navbar and revise the link titles, rename Projects directory to Portfolio

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -4,11 +4,13 @@ import Link from 'next/link';
 const Navbar = () => {
     return(
         <nav className='container fixed'>
-        <div className="max-w-screen-xl flex flex-wrap justify-between mx-auto p-4 text-lg" >
+        <div className="max-w-screen-xl flex flex-wrap justify-between mx-auto p-4 text-lg columns-2" >
+            <div> 
+            </div>
             <div className='space-x-10 ml-10' >
+                <Link href='/'>Home</Link>
                 <Link href='/about'> About </Link>
-                <Link href='/projects'>Projects</Link>
-                <button>Experience</button>
+                <Link href='/portfolio'>Portfolio</Link>
                 <button>Contact</button>
                 <button>Resume</button>
             </div>
