a300a7c69bdac1d73f47fd65b68adcea03d300fa
JordanJoseph <jordant.joseph@outlook.com>
2023-08-19 07:20:09 -0600

    resize last two images in app/images, update file names to deactivate the routes, update all file imports

--- src/app/images.jsx
+++ src/app/images.jsx
@@ -6,8 +6,8 @@ const Images = () => {
             <img className='object-scale-down w-2/5' src='./hiking.jpeg' alt='hiking'/>
         </div>
         <div className='flex justify-end bg-brown'>
-            <img className='object-scale-down w-80' src='./mountain.jpeg' alt='hiking'/>
-            <img className='object-scale-down w-80' src='./plane.jpeg' alt='hiking'/>
+            <img className='object-scale-down w-60' src='./mountain.jpeg' alt='hiking'/>
+            <img className='object-scale-down w-60' src='./plane.jpeg' alt='hiking'/>
         </div>
         </div>
     )
