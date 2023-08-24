9e5f7741f5bd5a10d5167af45215c608c13804e9
JordanJoseph <jordant.joseph@outlook.com>
2023-08-24 09:28:19 -0600

    change font size in experience component, change project links to github logo and add links to the detail component

--- src/app/components/portfolio/experience.jsx
+++ src/app/components/portfolio/experience.jsx
@@ -8,32 +8,32 @@ const Experience = () => {
         <tbody>
             <tr className="border-b">
                 <td>Student - Flatiron School</td>
-                <td className='text-right'>03/2023</td>
+                <td className='text-right text-sm'>03/2023</td>
                 <td className='text-right'>Colorado Springs, CO</td>
             </tr>
             <tr className="border-b">
                 <td>Staff Accountant - Bradshaw & Associates, PLLC</td>
-                <td className='text-right'>06/2021 - 05/2022</td>
+                <td className='text-right text-sm'>06/2021 - 05/2022</td>
                 <td className='text-right'>Colorado Springs, CO</td>
             </tr>
             <tr className="border-b">
-                <td>Income Auditor - the Broadmoor</td>
-                <td className='text-right'>10/2020 - 02/2021</td>
+                <td>Income Auditor - The Broadmoor</td>
+                <td className='text-right text-sm'>10/2020 - 02/2021</td>
                 <td className='text-right'>Colorado Springs, CO</td>
             </tr>
             <tr className="border-b">
                 <td>Executive Assistant - Papillon Helicopters</td>
-                <td className='text-right'>10/2019 - 05/2020</td>
+                <td className='text-right text-sm'>10/2019 - 05/2020</td>
                 <td className='text-right'>Tusayan, AZ</td>
             </tr>
             <tr className="border-b">
                 <td>Instructor - Avid4 Aventure</td>
-                <td className='text-right'>06/2019 - 09/2019</td>
+                <td className='text-right text-sm'>06/2019 - 09/2019</td>
                 <td className='text-right'>Burlingame, CA</td>
             </tr>
             <tr>
                 <td>Student - Colorado State University - Pueblo</td>
-                <td className='text-right'>05/2015</td>
+                <td className='text-right text-sm'>05/2015</td>
                 <td className='text-right'>Pueblo, CO</td>
             </tr>
         </tbody>
