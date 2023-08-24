9954d6b1c29da5491f23146234f13d3e3fe20d6d
JordanJoseph <jordant.joseph@outlook.com>
2023-08-23 22:14:07 -0600

    delete folders for main and about because routes are not being used and rename the files accordingly, create meow folder for new route and page file for component, update imports for file structure changes

---
+++ src/app/(components)/meow/page.js
@@ -0,0 +1,10 @@
+const Meow = () => {
+    return(
+        <main className='bg-fixed bg-center min-h-screen' style={{backgroundImage: "url(/swirls.jpg)"}}>
+            <img className='m-14 object-scale-down w-60' src='./sleepy_luc.jpeg' alt='cuteKitty' />
+            
+        </main>
+    )
+}
+
+export default Meow;
