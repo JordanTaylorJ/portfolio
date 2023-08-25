637c7e3ff2f9ea7318063b9b9101b5c40b92f2df
JordanJoseph <jordant.joseph@outlook.com>
2023-08-25 16:57:57 -0600

    changed text in contact, removed unused image files

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -2,12 +2,12 @@ const Contact = () => {
     return(
         <div className="container flex min-h-screen p-24 gap-24 column-2" >
             <div className='bg-greenblue px-10 py-14 self-center' >
-                <h1 className='text-3xl pb-8 text-white pl-5' >Get In Touch</h1>
+                <h1 className='text-3xl pb-8 text-white pl-5' >Let's Work Together!</h1>
                 <p className='text-center'>
-                I'm in search of a full-stack, frontend, or backend programming opportunity. I'm also open to collaborative projects.
+                I'm in search of a full-stack, frontend, or backend programming opportunity. I'm open to collaborative projects and excited to hear from you!
                 </p>
                 <br></br>
-                <div className='flex flex-col'>
+                <div className='flex flex-col text-lg'>
                     <a href='https://www.linkedin.com/in/jordantjoseph/' className='text-brown hover:text-white hover:bg-sage p-1 hover:rounded place-self-end'>Let's Connect</a>
                 </div>
             </div>
