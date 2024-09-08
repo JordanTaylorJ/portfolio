3a150e42eb2b01e0f58ceed1b3c62051af2af85f
JordanJoseph <jordant.joseph@outlook.com>
2024-09-08 09:00:21 -0600

    reformat experience table design and create list to map through so that none of the data is hard coded - ie, easier to edit list

--- src/app/components/portfolio/experience.jsx
+++ src/app/components/portfolio/experience.jsx
@@ -43,41 +43,15 @@ const Experience = () => {
         <div className='flex justify-center text-brown p-10'>
         <table className="w-full border-spacing-x-8 border-spacing-y-5">
         <tbody className='m-16 text-xs md:text-base'>
-            <tr className="p-20 border border-black border-t-0 border-l-0 border-r-0 ">
-                <td>Business Accountant - Carson Group Tax & Accounting</td>
-                <td className='collapse sm:visible text-right md:text-sm'>03/2024 - current</td>
-                <td className='collapse md:visible text-right'>Colorado Springs, CO</td>
-            </tr>
-            <tr className="border-b">
-                <td>Student - Flatiron School</td>
-                <td className='collapse sm:visible text-right md:text-sm'>03/2023</td>
-                <td className='collapse md:visible text-right'>Colorado Springs, CO</td>
-            </tr>
-            <tr className="border-b">
-                <td>Staff Accountant - Bradshaw & Associates, PLLC</td>
-                <td className='collapse sm:visible text-right md:text-sm'>06/2021 - 05/2022</td>
-                <td className='collapse md:visible text-right'>Colorado Springs, CO</td>
-            </tr>
-            <tr className="border-b">
-                <td>Income Auditor - The Broadmoor</td>
-                <td className='collapse sm:visible text-right md:text-sm'>10/2020 - 02/2021</td>
-                <td className='collapse md:visible text-right'>Colorado Springs, CO</td>
-            </tr>
-            <tr className="border-b">
-                <td>Executive Assistant - Papillon Helicopters</td>
-                <td className='collapse sm:visible text-right md:text-sm'>10/2019 - 05/2020</td>
-                <td className='collapse md:visible text-right'>Tusayan, AZ</td>
-            </tr>
-            <tr className="border-b">
-                <td>Instructor - Avid4 Aventure</td>
-                <td className='collapse sm:visible text-right md:text-sm'>06/2019 - 09/2019</td>
-                <td className='collapse md:visible text-right'>Burlingame, CA</td>
-            </tr>
-            <tr>
-                <td>Student - Colorado State University</td>
-                <td className='collapse sm:visible text-right md:text-sm'>05/2019</td>
-                <td className='collapse md:visible text-right'>Pueblo, CO</td>
-            </tr>
+            {list.map(exp => {
+                return(
+                    <tr className="p-20 border border-black border-t-0 border-l-0 border-r-0">
+                        <td>{exp.name}</td>
+                        <td className='collapse sm:visible text-right md:text-sm'>{exp.time}</td>
+                        <td className='collapse md:visible text-right'>{exp.location}</td>
+                    </tr>
+                )
+            })}
         </tbody>
       </table>
       </div>
