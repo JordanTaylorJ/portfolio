0a419a2d9257bf3491d9bd346ab2e42d8da79de4
JordanJoseph <jordant.joseph@outlook.com>
2025-02-12 07:35:42 -0700

    upgrade noded version & next.js version from 13 to 14. create route for webscraping blog data and a component to render that data.

---
+++ src/app/api/scraper/route.js
@@ -0,0 +1,4 @@
+export async function POST(request) {
+    return Response.json({message: 'hello world'})
+}
+
---
+++ src/app/components/portfolio/blog.jsx
@@ -0,0 +1,9 @@
+
+const Blog = () =>{
+
+    return(
+        <p>A link/reel of recent blog posts</p>
+    )
+}
+
+export default Blog;
