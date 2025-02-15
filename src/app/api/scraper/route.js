84c06b54f3f6ce77b5a4102489cc25f0c9af93b7
JordanJoseph <jordant.joseph@outlook.com>
2025-02-15 10:20:37 -0700

    create function to scrape blog posts and store data in object

--- src/app/api/scraper/route.js
+++ src/app/api/scraper/route.js
@@ -1,4 +1,22 @@
-export async function POST(request) {
-    return Response.json({message: 'hello world'})
-}
+import puppeteer from "puppeteer";
 
+const url = 'https://dev.to/jordantaylorj'
+
+const blogData = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const allArticles = await page.evaluate(() => {
+        const articles = document.querySelectorAll('.crayons-story')
+        return Array.from(articles).slice(0,3).map((article) => {
+            console.log(article)
+            const title = article.querySelector('a').innerText;
+            const url = article.querySelector('a').href;
+            return{title, url}
+        })
+    })
+    console.log(allArticles)
+}
+blogData();
+
+//export { blogData };
